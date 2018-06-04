e304a3a3ef635ed4a461013a75f6c0f871a7be31
Corentin Molitor <corentin.molitor@gmail.com>
2018-06-04 14:38:33 +0100

    Created function for running batch, corrected the usage

--- README.md
+++ README.md
@@ -1,19 +1,22 @@
 # run_pilon_batches.sh
-Script to run pilon by batches of contigs (to avoid out of memory issues)
+Script to run pilon by batches of sequences, develop to avoid out of memory issues.
+At the end, the script checks the number of batches and merge them into a corrected assembly.
 
 
 # Usage
 
-bash run_pilon_batches.sh -t <i>threads</i> -a <i>assembly.fasta</i> -b <i>batchSize</i> -f <i>"--frags file1.bam --frags file2.bam ..."</i> -p "<i>/path/to/pilon.jar</i>"
+bash run_pilon_batches.sh -t <i>threads</i> -a <i>assembly.fasta</i> -b <i>batchSize</i> -f <i>"--frags file1.bam --frags file2.bam ..."</i>  -o <i>"/path/to/outputDir"</i> -p <i>"/path/to/pilon.jar"</i>
 
 
 - The script needs a fasta fai file next to the assembly (you can create it using samtools faidx)
 
-- You need to put the quotes around the values for the -f and -p options, for the -f option you can put as any <i>--frags file.bam</i> as needed,
+- You need to put the quotes around the values for the -f and -p options, for the -f option you can put as any <i>--frags file.bam</i> as desired,
 
 - The bam (sorted and indexed) should be from an alignment against the -a <i>assembly.fasta</i> (cf pilon)
 
 # To implement 
 
-- More checks for user input
-- Add output directory option
+- Add memory setting option (now hard coded to 250 Gb)
+- More checks for user input (notably the --frags option)
+- Automatically create fasta.fai if not there
+- Add a possibility to perform more than one iteration (need to have access to reads and aligner)
--- run_pilon_batches.sh
+++ run_pilon_batches.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # TO ADD :
-#	- add OutputDir options (currently create batches in current folder)
 #	- create fata fai if not already there
-
+#	- add checks for frags
+#
 # Runs pilon in batches, uses the fasta fai from the assembly to get the scaffolds IDs (then uses pilon --targets option to launch pilon on these Ids)
 # Output is one folder for each batch
 
@@ -14,16 +14,43 @@ threads=20
 nostray="F"
 outputDir="./"
 
+
 function usage {
         echo "USAGE run_pilon_batches.sh -t [Threads] -a [Assembly fasta] -b [Batch size] -f [--frags align.bam] -o [Output directory] -p [pilon.jar] -n"
         echo "  -h Print this help message"
-        echo "  -t Number of threads to use (default: ${threads})"
+        echo "  -t Number of threads to use (default: 20)"
         echo "  -a Assembly in fasta format (required)"
-        echo "  -b batch size: how many sequences to process per pilon run (default: ${batchSize})"
+        echo "  -b batch size: how many sequences to process per pilon run (default: 100)"
         echo "  -f indicate the bam files location, format : '--frags /path/to/file1.bam --frags /path/to/file2.bam' (required)"
 	echo "	-o output directory (default: current directory)"
 	echo "  -p path to pilon jar file (required)"
-	echo "	-n use nostray with pilon, this skip the identification of stray pairs but decrease memory usage (default: ${nostray})"
+	echo "	-n use nostray with pilon, this skip the identification of stray pairs but decrease memory usage"
+}
+
+
+function do_batch {
+	batchNumber=$((${batchNumber}+1))
+
+	# To avoid having a comma at the beginning (and avoid pilon throwing an error)
+	batch=$(echo ${batch} | sed 's/^,//')
+
+
+	if [ ${nostray} == "T" ]; then
+		cmd="java -jar -Xmx250G ${pilonJar} --nostrays"
+	else
+		cmd="java -jar -Xmx250G ${pilonJar}"
+	fi
+
+	cmd="${cmd} --genome ${assemblyFasta} ${FRAGS} --output ${outputDir}/pilon_on_batch${batchNumber} --outdir ${outputDir}/pilon_on_batch${batchNumber}/ --changes --fix all --threads ${threads}$
+
+	echo ${cmd}
+	eval ${cmd}
+
+	batch=""
+	count=0
+
+	echo "BATCH ${batchNumber} Done !"
+	echo ""
 }
 
 
@@ -91,7 +118,7 @@ if [ ! -w ${outputDir} ]; then
 	usage
 	exit 1
 
-##### Add tests for other inputs (threads --frags etc...)
+##### Add tests for other inputs (--frags etc...)
 
 
 
@@ -113,29 +140,7 @@ for ID in ${scaffoldIDs}; do
 	# Launch Pilon every "batchSize" IDs
 	if [ ${count} -ge ${batchSize} ]; then
 
-		batchNumber=$((${batchNumber}+1))
-
-		# To avoid having a comma at the beginning (and avoid pilon throwing an error)
-		batch=$(echo ${batch} | sed 's/^,//')
-
-
-
-		if [ ${nostray} == "T" ]; then
-			cmd="java -jar -Xmx250G ${pilonJar} --nostrays"
-		else
-			cmd="java -jar -Xmx250G ${pilonJar}"
-		fi
-
-		cmd="${cmd} --genome ${assemblyFasta} ${FRAGS} --output ${outputDir}/pilon_on_batch${batchNumber} --outdir pilon_on_batch${batchNumber}/ --changes --fix all --threads ${threads} --targets '${batch}' > ${outputDir}/pilon_on_batch${batchNumber}.log"
-
-		echo ${cmd}
-		eval ${cmd}
-
-		batch=""
-		count=0
-
-		echo "BATCH ${batchNumber} Done !"
-		echo ""
+		do_batch
 	fi
 
 done
@@ -143,30 +148,20 @@ done
 echo "FINAL BATCH"
 # The last batch for the Ids left (not useful if number of scaffolds is modulo the batch size)
 if [ "$batch" != "" ]; then
-	batchNumber=$((${batchNumber}+1))
-
-	batch=$(echo ${batch} | sed 's/^,//')
-
-	cmd="java -jar -Xmx250G ${pilonJar} --genome ${assemblyFasta} ${FRAGS} --output pilon_on_batch${batchNumber} --outdir pilon_on_batch${batchNumber}/ --changes --fix all --threads ${threads} --targets '${batch}' > pilon_on_batch${batchNumber}.log"
-
-	echo ${cmd}
-	eval ${cmd}
-
-	echo "BATCH ${batchNumber} Done !"
-	echo ""
+	do_batch
 fi
 
 
 echo "All the batches have been processed"
 echo ""
-echo "Merging ${NbPilonFasta} fasta..."
+echo "Merging ${NbPilonFasta} fasta files..."
 
 # Merge fasta here
-NbPilonFasta=$(find ./pilon_on_batch* -maxdepth 1 -name "*.fasta" | wc -l)
+NbPilonFasta=$(find ${outputDir}/pilon_on_batch* -maxdepth 1 -name "*.fasta" | wc -l)
 if [ ${NbPilonFasta} -eq ${batchNumber} ]; then
 	prefix="pilon_corrected_assembly"
-	cat ./pilon_on_batch*/*.fasta > ${prefix}.fasta
-	echo "Done, the corrected assembly is: ${prefix}.fasta"
+	cat ${outputDir}/pilon_on_batch*/*.fasta > ${outputDir}${prefix}.fasta
+	echo "Done, the corrected assembly is: ${outputDir}/${prefix}.fasta"
 else
 	echo "Wrong number of fasta!"
 	echo "Pilon should have created ${batchNumber} fasta, we found ${NbPilonFasta} fasta..."
